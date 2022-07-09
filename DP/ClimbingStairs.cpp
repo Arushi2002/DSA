6cf216a60f8f04229464c1e97afea9afa6beba1b
Arushi2002 <79096912+Arushi2002@users.noreply.github.com>
2022-07-09 20:40:42 +0530

    09-07-2022
    https://leetcode.com/problems/climbing-stairs/

---
+++ DP/ClimbingStairs.cpp
@@ -0,0 +1,35 @@
+/* https://leetcode.com/problems/climbing-stairs/ */
+class Solution {
+public:
+    //DP Bottom Up
+    int climbStairs(int n) {
+        int arr[n+1];
+        if(n==1)
+            return 1;
+        if(n==2)
+            return 2;
+        if(n==3)
+            return 3;
+        arr[0]=0;
+        arr[1]=1;
+        arr[2]=2;
+        arr[3]=3;
+        for(int i=4;i<n+1;i++)
+        {
+            arr[i]=arr[i-1]+arr[i-2];
+        }
+        return arr[n];
+    }
+    //Another method is recursion +memoization
+  
+    //Normal doesnt work as it has O(n2) complexity
+    /*
+    int climbStairs(int n) {
+        if(n==1)
+            return 1;
+        if(n==2)
+            return 2;
+        
+        return climbStairs(n-1)+climbStairs(n-2);
+    }*/
+};

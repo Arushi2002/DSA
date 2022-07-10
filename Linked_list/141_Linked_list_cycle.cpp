72ad56562ed3017414f8dfb3d61862659ada936b
Arushi2002 <79096912+Arushi2002@users.noreply.github.com>
2022-07-11 00:08:56 +0530

    11-07-2022
    https://leetcode.com/problems/linked-list-cycle/

---
+++ Linked_list/141_Linked_list_cycle.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    /* Brute force not working idk. But in java its working
+    //Brute Force
+    bool hasCycle(ListNode* head) {
+        unordered_set<ListNode> nodesSeen;
+        while (head != NULL) {
+            if (nodesSeen.find(head)!=nodesSeen.end()) {
+                return true;
+            } else {
+                nodesSeen.insert(head);
+            }
+            head->head.next;
+        }
+        return false;
+    }
+};*/
+    
+    //Slow and Fast pointer method or 2 pointer method 
+    bool hasCycle(ListNode *head) {
+	
+		// if head is NULL then return false;
+        if(head == NULL)
+            return false;
+        
+		// making two pointers fast and slow and assignning them to head
+        ListNode *fast = head;
+        ListNode *slow = head;
+        
+		// till fast and fast-> next not reaches NULL
+		// we will increment fast by 2 step and slow by 1 step
+        while(fast != NULL && fast ->next != NULL)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+            
+			
+			// At the point if fast and slow are at same address
+			// this means linked list has a cycle in it.
+            if(fast == slow)
+                return true;
+        }
+        
+		// if traversal reaches to NULL this means no cycle.
+        return false;
+    }
+
+};

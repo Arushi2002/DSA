de19dd549cf93b9001c5829648f0edadbf40d43f
Arushi2002 <79096912+Arushi2002@users.noreply.github.com>
2022-07-28 23:40:48 +0530

    28-07-2022

---
+++ Sorting/Permuation2arrayssuchthatsumeisgreaterthanorequaltok.cpp
@@ -0,0 +1,14 @@
+/*https://www.geeksforgeeks.org/permute-two-arrays-sum-every-pair-greater-equal-k/ */
+class Solution {
+  public:
+    bool isPossible(long long a[], long long b[], int n, long long k) {
+        // Your code goes here
+        sort(a,a+n);
+        sort(b,b+n,greater<long long>());
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]+b[i]<k) return false;
+        }
+        return true;
+    }
+};
